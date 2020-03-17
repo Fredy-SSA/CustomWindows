080c322bb42bd1f10e3cad028d04f4d301ba26c7
Fredy-SSA <alin@zizo.ro>
2020-03-17 15:49:23 +0200

    asf

--- Ziua2.ps1
+++ Ziua2.ps1
@@ -11,5 +11,20 @@ Get-Service | ? {$_.status -like "st*"  -and  $_.StartType -like "Automatic"} | 
 Get-Command *fire*
 help set-NetFirewallProfile -ShowWindow
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Get-NetFirewallProfile  | select Name ,      enabled
+Get-NetFirewallProfile | select Name , enabled
+
+Get-NetFirewallProfile | export-csv c:\test.csv
+Get-NetFirewallProfile | select Name , enabled | export-csv c:\test.csv
+
+Get-NetFirewallProfile | gm
+Get-NetFirewallProfile | select Name , enabled | gm
+Get-NetFirewallProfile | select Name , enabled
+notepad c:\test.csv
+
+
+#display ip data 
+Get-Command get*ip*
+Get-NetIPAddress  | select * -First 1
+Get-NetIPAddress  |  select InterfaceAlias, IPAddress , IPv4Address , IPv6Address, hea*
+
 
