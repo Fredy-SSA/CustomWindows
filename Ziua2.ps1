78f849dc728564e59eb5f8dcf96aaaad5661fb53
Fredy-SSA <alin@zizo.ro>
2020-03-17 15:18:11 +0200

    all

--- Ziua2.ps1
+++ Ziua2.ps1
@@ -8,15 +8,8 @@ Get-Service | select * -First 1
 Get-Service | ? {$_.status -like "st*"  -and  $_.StartType -like "Automatic"} | Start-Service 
 
 #disable firewall winCore
+Get-Command *fire*
+help set-NetFirewallProfile -ShowWindow
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Get-NetFirewallProfile  | select Name , enabled
 
-
-#startBackup
- #Backup
- Get-Command *backup* 
- Get-Command *backup* -Module WindowsServerBackup
- get-help Start-WBBackup  -ShowWindow
-
- help New-WBPolicy -ShowWindow
- $Policy = Get-WBPolicy
- Start-WBBackup -Policy $Policy 
