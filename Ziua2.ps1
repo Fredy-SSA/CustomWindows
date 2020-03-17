666769f316090695578f4344053d02a2ac03dd1d
Fredy-SSA <alin@zizo.ro>
2020-03-17 14:40:23 +0200

    sdf

--- Ziua2.ps1
+++ Ziua2.ps1
@@ -6,3 +6,17 @@ Enable-PSRemoting -Force
 Get-Service | ? {$_.status -like "st*"  -and  $_.StartType -like "Automatic"}
 Get-Service | select * -First 1
 Get-Service | ? {$_.status -like "st*"  -and  $_.StartType -like "Automatic"} | Start-Service 
+
+#disable firewall winCore
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+
+#startBackup
+ #Backup
+ Get-Command *backup* 
+ Get-Command *backup* -Module WindowsServerBackup
+ get-help Start-WBBackup  -ShowWindow
+
+ help New-WBPolicy -ShowWindow
+ $Policy = Get-WBPolicy
+ Start-WBBackup -Policy $Policy 
