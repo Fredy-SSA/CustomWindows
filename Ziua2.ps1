6af011e143ddd7d032bbf7a76bca012f1ecc3f37
Fredy-SSA <alin@zizo.ro>
2020-03-17 11:36:01 +0200

    dfg

--- Ziua2.ps1
+++ Ziua2.ps1
@@ -1,1 +1,8 @@
+#powershell remote
 Enable-PSRemoting -Force
+
+
+#verifici serviciile oprite
+Get-Service | ? {$_.status -like "st*"  -and  $_.StartType -like "Automatic"}
+Get-Service | select * -First 1
+Get-Service | ? {$_.status -like "st*"  -and  $_.StartType -like "Automatic"} | Start-Service 
