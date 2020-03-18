823f5902e9f5ada07f7223ec79c72355cefe034f
Fredy-SSA <alin@zizo.ro>
2020-03-18 13:40:40 +0200

    all

--- Ziua3.ps1
+++ Ziua3.ps1
@@ -4,12 +4,10 @@ Enable-PSRemoting -Force
 #get VmName
 Get-VM
 
-
 Get-Command *network*
 Get-Command set*network*
 Get-Command set*ip*
 
-#Set IP Address cmdlets
 help Set-NetIPAddress -Examples 
 
 get-NetIPAddress | select * -First 1
@@ -20,31 +18,15 @@ help New-NetIPAddress -Examples
 
 Remove-NetIPAddress -InterfaceIndex 6 -IPAddress 192.168.0.1
 New-NetIPAddress -InterfaceIndex 6 -IPAddress 192.168.0.4 -PrefixLength 23
+
 Set-NetIPInterface -InterfaceIndex 6 -Dhcp Enabled
 
 
-get-NetIPInterface
-Set-NetIPInterface -InterfaceAlias iptest -Dhcp Enabled
+
+#ipv6
+<#
 
 
-#Powershell for Network
-Get-NetAdapter
-Restart-NetAdapter
-Get-NetIPInterface
-Get-NetIPAddress
-Get-NetRoute
-Get-NetConnectionProfile
-Get-DNSClientCache
-Get-DNSClientServerAddress
-Register-DnsClient
-Set-DnsClient
-Set-DnsClientGlobalSetting
-Set-DnsClientServerAddress
-Set‑NetIPAddress
-Set‑NetIPv4Protocol
-Set‑NetIPInterface
-Test-Connection
-Test-NetConnection
-Resolve-Dnsname
 
 
+#>
