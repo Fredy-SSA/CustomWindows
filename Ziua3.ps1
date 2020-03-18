0a6f5da2febfe327ec826eaa18f78c169d55eb31
Fredy-SSA <alin@zizo.ro>
2020-03-18 09:55:26 +0200

    all

--- Ziua3.ps1
+++ Ziua3.ps1
@@ -13,5 +13,12 @@ help Set-NetIPAddress -Examples
 get-NetIPAddress | select * -First 1
 New-NetIPAddress -InterfaceIndex 6 -IPAddress 192.168.0.4 -PrefixLength 23
 
+help New-NetIPAddress -Examples 
+
+
+Remove-NetIPAddress -InterfaceIndex 6 -IPAddress 192.168.0.1
+New-NetIPAddress -InterfaceIndex 6 -IPAddress 192.168.0.4 -PrefixLength 23
+
+Set-NetIPInterface -InterfaceIndex 6 -Dhcp Enabled
 
 
