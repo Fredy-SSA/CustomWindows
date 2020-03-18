95e8eaeb8fff5e54e128bd745f9cc9688303818a
Fredy-SSA <alin@zizo.ro>
2020-03-18 09:58:13 +0200

    adsf

--- Ziua3.ps1
+++ Ziua3.ps1
@@ -4,10 +4,12 @@ Enable-PSRemoting -Force
 #get VmName
 Get-VM
 
+
 Get-Command *network*
 Get-Command set*network*
 Get-Command set*ip*
 
+#Set IP Address cmdlets
 help Set-NetIPAddress -Examples 
 
 get-NetIPAddress | select * -First 1
@@ -18,7 +20,9 @@ help New-NetIPAddress -Examples
 
 Remove-NetIPAddress -InterfaceIndex 6 -IPAddress 192.168.0.1
 New-NetIPAddress -InterfaceIndex 6 -IPAddress 192.168.0.4 -PrefixLength 23
-
 Set-NetIPInterface -InterfaceIndex 6 -Dhcp Enabled
 
+
+get-NetIPInterface
+Set-NetIPInterface -InterfaceAlias iptest -Dhcp Enabled
 
