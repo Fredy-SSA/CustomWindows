9e3679a88707338e0cc9d47f9c1bceebe48f17e9
Fredy-SSA <alin@zizo.ro>
2020-03-19 10:14:38 +0200

    3.19

---
+++ Ziua4.ps1
@@ -0,0 +1,7 @@
+﻿#storage
+Get-StoragePool   | select *
+Get-Command -Module  Storage -Verb get
+Get-PhysicalDisk  | select *
+Get-StoragePool | ? FriendlyName -like "PoolStorageUseri" | Get-PhysicalDisk   | select *
+
+revenim la 10:25
