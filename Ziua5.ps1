cee63cb9f8286ae4806c10257dcad480068100d4
Fredy-SSA <alin@zizo.ro>
2020-03-20 14:26:08 +0200

    all

--- Ziua5.ps1
+++ Ziua5.ps1
@@ -2,3 +2,18 @@
 #\\LON-DC1.Adatum.com\WhitnessShare
 Set-ClusterQuorum -FileShareWitness "\\LON-DC1.Adatum.com\WhitnessShare"
 get-ClusterQuorum
+
+
+#DFS 
+get-command -Module dfsn
+
+#ex comanda"
+New-DfsnRoot -TargetPath "\\Contoso-FS\AccountingResources" -Type DomainV2 -Path "\\Contoso\AccountingResources"
+
+#Creaza un director
+mkdir c:\testShare
+#fa un share
+New-SMBShare –Name "Test Share" –Path "c:\testShare" –FullAccess Everyone 
+
+#Creare DFSN root
+New-DfsnRoot -TargetPath "\\tm-vm1\Test Share" -Type DomainV2 -Path "\\Adatum.com\Test Share"
